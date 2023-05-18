222c4efa7747b6a945eca5d6eba2622b4416fdd8
Ma1y0 <matyas.barr@gmail.com>
2023-05-18 08:45:13 +0200

    Harpoon

--- src/battery.rs
+++ src/battery.rs
@@ -5,9 +5,9 @@ pub enum Status {
     Discharging
 }
 
-pub fn get_battery_level() -> Result<(String, Status), std::io::Error>{
+pub fn get_battery_data() -> Result<(String, Status), std::io::Error>{
      let data = read_to_string("/sys/class/power_supply/BAT1/capacity")?;
-     let status = match read_to_string("/sys/class/power_supply/BAT1/status")? {
+     let status = match read_to_string("/sys/class/power_supply/BAT1/status")?.as_str() {
          "Discharging" => Status::Discharging,
          "Charging" => Status::Charging,
          _ => Status::Charging
